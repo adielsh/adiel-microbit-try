f700fd9af72f8fad7a89e1a069903bcbd3189b0e
adielsh <16068359+adielsh@users.noreply.github.com>
2022-12-07 17:05:42 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,16 @@
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 4; index++) {
+        servos.P0.setAngle(90)
+        led.plot(3, 4)
         basic.showLeds(`
-            # # . . #
-            # # # . .
-            . # . # .
-            # # # . .
+            # # # . #
             . . . . .
+            # # . # .
+            # # # . .
+            # . . . .
             `)
     }
 })
 input.onButtonPressed(Button.A, function () {
-    led.plot(3, 4)
+	
 })
